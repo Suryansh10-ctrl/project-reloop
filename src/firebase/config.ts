813bf9dad3d41dbe5bef00c5e834dc35d5d5a55a
Ritik Thakur <rohitthakur2006rt2006@gmail.com>
2025-10-02 20:43:27 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9856810569-ede17",
+  "appId": "1:1022464157843:web:7dc6af0be645eb513bc566",
+  "apiKey": "AIzaSyAISl73h4PSKPGV-Y4NJSs0eM_34hstRIA",
+  "authDomain": "studio-9856810569-ede17.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1022464157843"
+};
